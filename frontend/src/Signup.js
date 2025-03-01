12f1498d14a4e449026f83421912ecd9f4f736fd
Ziya Rashid <144579482+ziyarashidd@users.noreply.github.com>
2025-03-01 17:39:34 +0530

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -113,6 +113,7 @@ const Signup = () => {
                                 </div>
                                 <div className="col-md-12 d-flex justify-content-center">
                                     <button type="submit" className="mybtn text-light p-2 rounded-2 border border-0 w-100">Register</button>
+                                            window.location.href = 'https://youtube-note.vercel.app/';  // Redirect to external URL
                                 </div>
                             </form>
                             <div className="mt-3">
